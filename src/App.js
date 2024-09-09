b6e320a73e091c6b8933bdda0868e19a33d81410
vignesh <vigneshvicky242002@gmail.com>
2024-09-09 21:06:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import img1 from './images/login.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Save
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="background">
+        
+      </div>
+      <form>
+        <h3>Login <span>Here</span></h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username"></input>
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password"></input>
+
+        <button >Log In</button>
+        <div className="social">
+          <div className="go"><i class="fab fa-google"></i>  Google</div>
+          <div className="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+        <div className="register">
+          <p>New to Website?</p><a href="">Register</a>
+          <button>Change</button>
+        </div>
+      </form>
     </div>
   );
 }
