99329ef817be2740dad66dda3fe68cc3d3cc37e2
vignesh <vigneshvicky242002@gmail.com>
2024-09-10 20:09:59 +0530

    refactor:message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import img1 from './images/login.jpg'
+
 function App() {
   return (
     <div>
@@ -10,13 +10,12 @@ function App() {
       <form>
         <h3>Login <span>Here</span></h3>
 
-        <label for="username">Username</label>
+        <label for="username">User</label>
         <input type="text" placeholder="Email or Phone" id="username"></input>
 
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password"></input>
+        
 
-        <button >Log In</button>
+        <button >Log In With OTP</button>
         <div className="social">
           <div className="go"><i class="fab fa-google"></i>  Google</div>
           <div className="fb"><i class="fab fa-facebook"></i>  Facebook</div>
